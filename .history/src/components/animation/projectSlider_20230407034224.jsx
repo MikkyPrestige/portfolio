56fe469c38f3c317455a86c87469f103debafbf5
Mikky <meekyberry6@gmail.com>
2023-04-07 07:42:25 +0100

    feat: added more animations

---
+++ .history/src/components/animation/projectSlider_20230407034224.jsx
@@ -0,0 +1,160 @@
+import { React, useState, useEffect, useRef } from "react";
+import Avatar from "../avatar";
+import Play from "../../assets/images/play.svg";
+import Pause from "../../assets/images/pause.svg";
+import { FaForward } from "react-icons/fa";
+import { FaBackward } from "react-icons/fa";
+import { Fade } from "react-awesome-reveal";
+
+const ProjectSlider = ({
+  projects,
+  autoPlay = false,
+  slideInterval = 3000,
+  onNextSlide,
+}) => {
+  const [currentProject, setCurrentProject] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(autoPlay);
+  const [isHovered, setIsHovered] = useState("");
+  const slideRef = useRef(null);
+
+  // FUNCTION TO HANDLE NEXT SLIDE
+  const handleNextSlide = () => {
+    setCurrentProject(
+      currentProject === projects.length - 1 ? 0 : currentProject + 1
+    );
+  };
+
+  // FUNCTION TO HANDLE PREVIOUS SLIDE
+  const handlePreviousSlide = () => {
+    setCurrentProject(
+      currentProject === 0 ? projects.length - 1 : currentProject - 1
+    );
+  };
+
+  // FUNCTION TO HANDLE PLAY/PAUSE
+  const handlePlayPause = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  useEffect(() => {
+    if (isPlaying) {
+      slideRef.current = setInterval(() => {
+        setCurrentProject(
+          currentProject === projects.length - 1 ? 0 : currentProject + 1
+        );
+      }, slideInterval);
+    } else {
+      clearInterval(slideRef.current);
+    }
+    return () => clearInterval(slideRef.current);
+  }, [isPlaying, slideInterval, currentProject, projects.length]);
+
+  useEffect(() => {
+    if (onNextSlide) {
+      onNextSlide(currentProject);
+    }
+  }, [currentProject, onNextSlide]);
+
+  return (
+    <div className="projectSlider">
+      <div className="projectSlider--contents">
+        <Fade duration={5000} direction="right">
+          <div className="projectSlider--contents__container">
+            <Avatar
+              image={projects[currentProject].image}
+              alt={projects[currentProject].title}
+              style={{
+                width: "100%",
+                height: "100%",
+                border: "1px solid #ededed",
+                borderRadius: "1rem",
+                objectFit: "cover",
+              }}
+            />
+          </div>
+          <div className="projectSlider--contents__details">
+            <h2 className="projectSlider--contents__details--title">
+              {projects[currentProject].title}
+            </h2>
+            <a
+              href={projects[currentProject].link}
+              target="_blank"
+              rel="noreferrer"
+              className="projectSlider--contents__details--link"
+            >
+              View Project
+            </a>
+          </div>
+        </Fade>
+      </div>
+      <div className="projectSlider--animation">
+        <Fade direction="left" duration={5000}>
+          <div className="projectSlider--controls">
+            <button
+              className="projectSlider--controls__btn"
+              onClick={handlePreviousSlide}
+            >
+              <FaBackward
+                style={{
+                  width: "3rem",
+                  height: "3rem",
+                  fill: "rgba(255, 0, 0, 0.5)",
+                }}
+              />
+            </button>
+            <button
+              className="projectSlider--controls__btn"
+              onClick={handlePlayPause}
+            >
+              {isPlaying ? (
+                <Avatar
+                  image={Pause}
+                  alt="Pause Icon"
+                  style={{ width: "100%", height: "100%" }}
+                />
+              ) : (
+                <Avatar
+                  image={Play}
+                  alt="Play Icon"
+                  style={{ width: "100%", height: "100%" }}
+                />
+              )}
+            </button>
+            <button
+              className="projectSlider--controls__btn"
+              onClick={handleNextSlide}
+            >
+              <FaForward
+                style={{ width: "100%", height: "100%", fill: "#1D976C" }}
+              />
+            </button>
+          </div>
+          {projects.length > 1 && (
+            <div className="projectSlider--pagination">
+              {projects.map((project, index) => (
+                <button
+                  key={index}
+                  className={`projectSlider--pagination__dot ${
+                    currentProject === index ? "active" : ""
+                  }`}
+                  onClick={() => setCurrentProject(index)}
+                  onMouseEnter={() => setIsHovered(index)}
+                  onMouseLeave={() => setIsHovered(-1)}
+                >
+                  {isHovered === index && (
+                    // Display tooltip for the only for particular dot hovered on
+                    <span className="projectSlider--pagination__dot--tooltip">
+                      {project.title}
+                    </span>
+                  )}
+                </button>
+              ))}
+            </div>
+          )}
+        </Fade>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSlider;
---
+++ .history/src/components/footer_20230407061326.jsx
@@ -0,0 +1,145 @@
+// FOOTER COMPONENT
+import React from "react";
+import Avatar from "./avatar";
+import CodeCup from "../assets/images/projects/coding-coffee.jpg"
+import Discord from "../assets/images/socials/discord.svg";
+import Github from "../assets/images/socials/icons8-github-94.png";
+import Linkedin from "../assets/images/socials/linkedin.png";
+import { Reveal } from "react-awesome-reveal";
+import {keyframes} from "@emotion/react";
+
+
+const Jello = keyframes`
+from,
+  11.1%,
+  to {
+    -webkit-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+  }
+
+  22.2% {
+    -webkit-transform: skewX(-11.5deg) skewY(-11.5deg);
+    transform: skewX(-11.5deg) skewY(-11.5deg);
+  }
+
+  33.3% {
+    -webkit-transform: skewX(5.25deg) skewY(5.25deg);
+    transform: skewX(5.25deg) skewY(5.25deg);
+  }
+
+  44.4% {
+    -webkit-transform: skewX(-3.125deg) skewY(-3.125deg);
+    transform: skewX(-3.125deg) skewY(-3.125deg);
+  }
+
+  55.5% {
+    -webkit-transform: skewX(1.5625deg) skewY(1.5625deg);
+    transform: skewX(1.5625deg) skewY(1.5625deg);
+  }
+
+  66.6% {
+    -webkit-transform: skewX(-0.78125deg) skewY(-0.78125deg);
+    transform: skewX(-0.78125deg) skewY(-0.78125deg);
+  }
+
+  77.7% {
+    -webkit-transform: skewX(0.390625deg) skewY(0.390625deg);
+    transform: skewX(0.390625deg) skewY(0.390625deg);
+  }
+
+  88.8% {
+    -webkit-transform: skewX(-0.1953125deg) skewY(-0.1953125deg);
+    transform: skewX(-0.1953125deg) skewY(-0.1953125deg);
+  }
+}`;
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer--line">
+        <p className="footer--line__content"></p>
+      </div>
+      <div className="footer--container animation-overflow">
+        <Reveal keyframes={Jello} duration={5000}>
+        <div className="footer--container__content">
+          <div className="footer--container__content--details">
+            <div className="footer--container__content--details__top">
+              <div className="footer--container__content--details__top--logo">
+                <Avatar
+                  image={CodeCup}
+                  alt="Laptop Image"
+                  style={{ width: "100%", height: "100%", borderRadius: "50%" }}
+                />
+              </div>
+              <p className="footer--container__content--details__top--email">
+                <a
+                  href="mailto: michaelelue117@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="footer--container__content--details__top--email--link"
+                >
+                  michaelelue117@gmail.com
+                </a>
+              </p>
+            </div>
+            <div className="footer--container__content--details__bottom">
+              <p className="footer--container__content--details__bottom--text">
+                I am a software developer with a passion for creating beautiful
+                and functional user interfaces.
+              </p>
+            </div>
+          </div>
+          <div className="footer--container__content--media">
+            <h2 className="footer--container__content--media--title">Media</h2>
+            <div className="footer--container__content--media--links">
+              <a
+                href="https://github.com/MeekyBerry"
+                target="_blank"
+                rel="noreferrer"
+                className="footer--container__content--media--links--link"
+              >
+                <Avatar
+                  image={Github}
+                  alt="GitHub Icon"
+                  style={{ width: "100%", height: "100%" }}
+                />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/mikkylanky/"
+                target="_blank"
+                rel="noreferrer"
+                className="footer--container__content--media--links--link"
+              >
+                <Avatar
+                  image={Linkedin}
+                  alt="GitHub Icon"
+                  style={{ width: "100%", height: "100%" }}
+                />
+              </a>
+              <a
+                href="https://discordapp.com/users/786382582220193803"
+                target="_blank"
+                rel="noreferrer"
+                className="footer--container__content--media--links--link"
+              >
+                <Avatar
+                  image={Discord}
+                  alt="GitHub Icon"
+                  style={{ width: "100%", height: "100%" }}
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="footer--container__copyright">
+          <p className="footer--container__copyright--text">
+            &copy; 2023. Made by Elue
+          </p>
+        </div>
+        </Reveal>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
