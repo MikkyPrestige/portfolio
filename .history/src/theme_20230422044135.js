d1add8627305514489a6c8bb5b8b6d8f5e9e5f3b
Mikky <meekyberry6@gmail.com>
2023-04-23 02:20:33 +0100

    feat: Added toggle for dark and light mode

---
+++ .history/src/theme_20230422044135.js
@@ -0,0 +1,29 @@
+const theme = {
+  config: {
+    useColorSchemeMediaQuery: "system",
+    useCustomProperties: true,
+    initialColorMode: "system",
+  },
+  colors: {
+    // text: "#1B1B2F",
+    text: "#333",
+    background: "#E8F4F8",
+    primary: "#2c3e50",
+    secondary: "#6c5ce7",
+    inverseText: "#000000",
+    highlight: "#00FF7F",
+    modes: {
+      dark: {
+        // text: "#D3D3D3",
+        text: "#fff",
+        background: "#1B1B2F",
+        primary: "#3498DB",
+        secondary: "#55efc4",
+        inverseText: "#FFFFFF",
+        highlight: "#F7DC6F",
+      },
+    }
+  },
+};
+
+export default theme;
---
+++ .history/src/theme_20230422201004.js
@@ -0,0 +1,27 @@
+const theme = {
+  config: {
+    useColorSchemeMediaQuery: "system",
+    useCustomProperties: true,
+    initialColorMode: "system",
+  },
+  colors: {
+    text: "#1B1B2F",
+    background: "#E8F4F8",
+    primary: "#2c3e50",
+    secondary: "#6c5ce7",
+    inverseText: "#000000",
+    highlight: "#00FF7F",
+    modes: {
+      dark: {
+        text: "#D3D3D3",
+        background: "radial-gradient(100% 100% at 50% 100%, var(--color--bg-dark-2) 21.52%, var(--color--bg-dark) 100%);",
+        primary: "#3498DB",
+        secondary: "#55efc4",
+        inverseText: "#FFFFFF",
+        highlight: "#F7DC6F",
+      },
+    }
+  },
+};
+
+export default theme;
