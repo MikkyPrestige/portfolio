d1cbd8ee86c2f59c3b014a316d80c6c8121c5987
Mikky <meekyberry6@gmail.com>
2023-05-30 23:15:17 +0100

    Removing ignored files

--- src/pages/token.js
+++
@@ -1,6 +0,0 @@
-import MapBoxgl from "mapbox-gl";
-
-MapBoxgl.accessToken =
-  "pk.eyJ1IjoibWlra3lsYW5reSIsImEiOiJjbGV2dzJkYmsyNGNtM3JwNHQwM2JhZW54In0.lnfhNIrn8tfvMUYXyCFOGA";
-
-export const mapboxgl = MapBoxgl;
