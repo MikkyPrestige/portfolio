c7d476482314632b5c464bd4861492b129183756
Mikky <meekyberry6@gmail.com>
2023-09-26 03:10:53 +0100

    feat: Edit 'StringRefine' project description

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -471,10 +471,7 @@ const Projects = () => {
                     displays the entered string as cards. Each card displays a
                     single character from the string, and each character's
                     duplicate instances can be removed by clicking the delete
-                    icon. If a character has no duplicates, it will remain on
-                    the screen. If there are no duplicate characters in the
-                    string, a success header is displayed. The original and
-                    updated strings are also displayed.
+                    icon.
                   </p>
                 </div>
                 <div className="project--card__links">
